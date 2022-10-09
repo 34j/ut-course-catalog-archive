5868e98324690caf58a4dfba62f0093fceb0b39e
34j <55338215+34j@users.noreply.github.com>
2022-10-10 01:03:34 +0900

    Add requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-requirements = ["click>=7.0", "beautifulsoup4>=4.8", "aiohttp>=3.8", "tenacity>=8.1", "aiofiles>=0.7"]
+requirements = ["click>=7.0", "beautifulsoup4>=4.8", "aiohttp>=3.8", "tenacity>=8.1", "aiofiles>=0.7", "tqdm"]
 
 test_requirements = []
 
